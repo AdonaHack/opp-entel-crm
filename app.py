c75a89610eab8b1341eb5df2306f678d3af9057d
AdonaHack <casafamcelis2025@gmail.com>
2026-09-24 10:14:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import os
 from flask import Flask, jsonify, render_template, request
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -91,4 +92,5 @@ def registrar_opp():
 
 
 if __name__ == "__main__":
-  app.run(debug=True, port=5000)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
